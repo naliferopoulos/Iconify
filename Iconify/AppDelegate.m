4761aa595f84b5a058f8c60e328ae7240efa852b
naliferopoulos <aliferopoulos@icloud.com>
2017-06-04 20:26:32 +0300

    Added Alert Features

--- Iconify/AppDelegate.m
+++ Iconify/AppDelegate.m
@@ -11,6 +11,11 @@
 @interface AppDelegate ()
 @property (strong, nonatomic) NSStatusItem *iconify;
 @property (strong, nonatomic) NSDictionary *settings;
+@property (strong, nonatomic) NSString *text;
+@property (strong, nonatomic) NSString *subText;
+@property (strong, nonatomic) NSString *exit;
+@property (strong, nonatomic) NSString *display;
+@property (strong, nonatomic) NSAlert *alert;
 @end
 
 @implementation AppDelegate
@@ -18,6 +23,8 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     self.iconify = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    
+    [_iconify setAction:@selector(itemClicked:)];
     
     NSFileManager *fileManager = [NSFileManager defaultManager];
     
@@ -29,16 +36,44 @@
     if ([fileManager fileExistsAtPath: @"/Library/Application Support/Iconify/settings.plist"])
     {
         _settings = [[NSDictionary alloc] initWithContentsOfFile: @"/Library/Application Support/Iconify/settings.plist"];
-        _iconify.toolTip = [_settings objectForKey:@"Text"];
+        _iconify.toolTip = [_settings objectForKey:@"Tooltip"];
+        _text = [_settings objectForKey:@"Text"];
+        _subText = [_settings objectForKey:@"Subtext"];
+        _display = [_settings objectForKey:@"Display"];
+        _exit = [_settings objectForKey:@"Exit"];;
     }
     
     [_iconify.image setTemplate:YES];
     
     _iconify.highlightMode = NO;
     
+    _alert = [[NSAlert alloc] init];
+    [_alert setMessageText: _text];
+    [_alert setInformativeText: _subText];
+    [_alert addButtonWithTitle: @"Ok"];
+    
+    if ([fileManager fileExistsAtPath: @"/Library/Application Support/Iconify/iconify_large.png"])
+    {
+        [_alert setIcon: [[NSImage alloc] initWithContentsOfFile: @"/Library/Application Support/Iconify/iconify_large.png"]];
+    }
     
 }
 
+- (void)itemClicked:(id)sender
+{
+    NSEvent *event = [NSApp currentEvent];
+    if([event modifierFlags] & NSEventModifierFlagControl)
+    {
+        if([_exit  isEqual: @"TRUE"])
+        {
+            [[NSApplication sharedApplication] terminate:self];
+            return;
+        }
+    }
+    
+    if([_display  isEqual: @"TRUE"])
+        [_alert runModal];
+}
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification
 {
--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 ## How does it work?
 *Iconify* is an agent application (that means it runs without an app icon). It places an icon of your choice (usually 20*20 pixels, so that it fits) on the status bar. It can also display a custom tooltip that you choose when hovering over the icon.
 ## How do I customize it?
-*Iconify* needs two main files to run:
-* iconify.png  - The icon that is displayed
-* settings.plist  - A property list containing the key **Text**, the value of which is diplayed as a tooltip.
-Both files need to be in /Library/Application Support/Iconify. Just create the folder and place them in! Simple as that! :)
+*Iconify* needs three main files to run:
+* iconify.png  - The icon that is displayed.
+* iconify_large.png - The icon that is displayed on the notification when clicked.
+* settings.plist  - A property list containing the following keys:
+    ** [String]Tooltip - The tooltip displayed when hovering over the icon.
+    ** [String]Text - The text of the notification that is displayed when clicking the icon.
+    ** [String]Subtext - The text of the notification that is displayed when clicking the icon.
+    ** [String]Display - Determines wether a notification should display when clicking the icon.
+    ** [String]Exit - Determines wether Iconify can be shut down using control+click.
+All files need to be in /Library/Application Support/Iconify. Just create the folder and place them in! Simple as that! :)
 
 ### Iconify is made with <3 and Xcode by Nick Aliferopoulos (aliferopoulos@icloud.com)
